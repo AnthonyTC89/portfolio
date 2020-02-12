eaf0767b6bf8250867418a94b0d53c4c7380e6e5
AnthonyTC89 <ptonyptc@gmail.com>
2020-02-11 20:13:27 -0500

    Experience section styled

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -2,16 +2,19 @@ import React from 'react';
 import uuidv4 from 'uuid/v4';
 import profile from '../Profile.json';
 import 'bootstrap/dist/css/bootstrap-grid.css';
+import './Experience.css';
 
 const Experience = () => (
   <section id="Experience" className="container">
-    <h2>Work Experience</h2>
-    <div className="row">
+    <h2 className="header-experience">Work Experience</h2>
+    <div className="row row-experience">
       {profile.experience.map((exp) => (
-        <article key={uuidv4()} className="col-12 col-sm-6">
-          <h4>{exp.date}</h4>
-          <h3>{exp.company}</h3>
-          <span>{exp.position}</span>
+        <article key={uuidv4()} className="col-12 col-sm-6 experience">
+          <h6>{exp.date}</h6>
+          <div className="company">
+            <h4>{exp.company}</h4>
+          </div>
+          <strong>{exp.position}</strong>
           <p>{exp.goals}</p>
         </article>
       ))}

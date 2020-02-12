c0a6cd4630db04ce6d497a218b2750ee8fd73206
AnthonyTC89 <ptonyptc@gmail.com>
2020-02-11 20:43:20 -0500

    Education section styled

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -2,16 +2,19 @@ import React from 'react';
 import uuidv4 from 'uuid/v4';
 import profile from '../Profile.json';
 import 'bootstrap/dist/css/bootstrap-grid.css';
+import './Education.css';
 
 const Education = () => (
   <section id="Education" className="container">
-    <h2>Education</h2>
-    <div className="row">
+    <h2 className="header-education">Education</h2>
+    <div className="row row-education">
       {profile.education.map((edu) => (
-        <article key={uuidv4()} className="col-12 col-sm-6">
-          <h4>{edu.date}</h4>
-          <h3>{edu.institution}</h3>
-          <span>{edu.grade}</span>
+        <article key={uuidv4()} className="col-12 col-sm-6 education">
+          <h6>{edu.date}</h6>
+          <div className="institution">
+            <h4>{edu.institution}</h4>
+          </div>
+          <strong>{edu.grade}</strong>
           <p>{edu.goals}</p>
         </article>
       ))}

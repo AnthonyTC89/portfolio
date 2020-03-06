30f6b35bef5ec5b78aea9901940d6bc8ba5af835
AnthonyTC89 <ptonyptc@gmail.com>
2020-03-05 22:17:50 -0500

    url connection MongoDB updated

--- src/database.js
+++ src/database.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://anthonytc89:alsimmambla@portfolio-atc-n69h1.mongodb.net/profile?retryWrites=true&w=majority';
+const USERNAME = process.env.USERNAME_MONGODB;
+const PASSWORD = process.env.PASSWORD_MONGODB;
+
+const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@portfolio-atc-n69h1.mongodb.net/profile?retryWrites=true&w=majority`;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Success Connection MongoDB Atlas');

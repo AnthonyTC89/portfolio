4f0bcce4c124b37515b9b48c42ccd9411ec304c1
AnthonyTC89 <ptonyptc@gmail.com>
2020-03-11 14:38:37 -0500

    Footer added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import profile from '../Profile.json';
+import { Social } from '../Icons.json';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Footer.css';
 
-const Footer = () => {
-  const fullName = `${profile.name} ${profile.lastname}`;
-  return (
-    <footer className="container">
-      {fullName}
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="container">
+    <div className="row row-footer">
+      <section className="copyright-section">
+        <small>AnthonyTC89 © 2020</small>
+      </section>
+      <section className="link-section">
+        <img src={Social.facebook} alt="facebook-icon" className="icon-social" />
+        <img src={Social.twitter} alt="twitter-icon" className="icon-social" />
+        <img src={Social.linkedin} alt="linkedin-icon" className="icon-social" />
+        <img src={Social.github} alt="github-icon" className="icon-social" />
+      </section>
+    </div>
+  </footer>
+);
 
 export default Footer;
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -5,6 +5,7 @@ import AboutMe from '../Components/AboutMe';
 import Experience from '../Components/Experience';
 import Projects from '../Components/Projects';
 import Education from '../Components/Education';
+import Footer from '../Components/Footer';
 
 const Home = () => (
   <>
@@ -15,6 +16,7 @@ const Home = () => (
       <Projects />
       <Experience />
       <Education />
+      <Footer />
     </main>
   </>
 );

2e2b5c7fb1cdff2802792bc0ddf1ca4d953e0a94
AnthonyTC89 <ptonyptc@gmail.com>
2020-03-16 09:33:47 -0500

    fix: SendEmail for two directions

--- src/routes/sendEmail.js
+++ src/routes/sendEmail.js
@@ -8,8 +8,18 @@ router.post('/sendEmail', async (req, res) => {
   const from = 'ptonyptc@gmail.com';
   const subject = 'Portfolio - autoReply';
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = { to, from, subject, text };
-  const result = await sgMail.send(msg);
+  const msgReply = { to,
+    from,
+    subject,
+    text: 'Thank you for writting, I will contact to you as soon as possible.' };
+  const msgOwner = {
+    to: from,
+    from: to,
+    subject: 'Portfolio contact',
+    text,
+  };
+  const result = await sgMail.send(msgReply);
+  await sgMail.send(msgOwner);
   res.json(result);
 });
 
